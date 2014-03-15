4e65093eb41982aa15c47dcda7ea07b2f5028605
chaeplin <chaeplin@gmail.com>
2014-03-15 15:43:10 +0900

    move SUBSIDY_FUNC repo use https://github.com/chaeplin/SUBSIDY_FUNC

--- py_module/darkcoin-subsidy-python/README.md
+++
@@ -1,6 +0,0 @@
-darkcoin-subsidy-python
-=====================
-
-Python module for p2poll-drk SUBSIDY_FUNC
-
-need boost-devel 
--- py_module/darkcoin-subsidy-python/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-
-setup(name="darkcoin_subsidys",
-    ext_modules=[
-        Extension("darkcoin_subsidy", ["darkcoin_GetBlockBaseValue.cpp"],
-        libraries = ["boost_python"])
-    ])
