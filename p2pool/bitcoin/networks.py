f77ee11b150f6deaffd844ae91aa9d517c326b6a
chaeplin <chaeplin@gmail.com>
2014-03-26 01:54:32 +0900

    add testnet and 45000 hardfork banner.

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -42,6 +42,30 @@ nets = dict(
         DUST_THRESHOLD=0.001e8,
     ),
 
+    darkcoin_testnet=math.Object(
+        P2P_PREFIX='fcc1b7dc'.decode('hex'),
+        P2P_PORT=19999,
+        ADDRESS_VERSION=111,
+        RPC_PORT=19998,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'darkcoinaddress' in (yield bitcoind.rpc_help()) and
+            (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda nBits, height: __import__('darkcoin_subsidy').GetBlockBaseValue(nBits, height),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('dark_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('dark_hash').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='tDRK',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Darkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Darkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.darkcoin'), 'darkcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
+
+
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -25,11 +25,32 @@ nets = dict(
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=False,
         WORKER_PORT=7903,
-        BOOTSTRAP_ADDRS='p2phash.com asia02.mine.nu asia01.mine.nu 157.56.161.11 54.186.8.140 62.141.39.175 mightypool.net 85.131.127.26 213.229.88.102 cryptohasher.net darkcoin.fr'.split(' '),
+        BOOTSTRAP_ADDRS='p2phash.com asia02.poolhash.org asia01.poolhash.org 157.56.161.11 54.186.8.140 62.141.39.175 mightypool.net 85.131.127.26 213.229.88.102 cryptohasher.net darkcoin.fr'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-drk',
         VERSION_CHECK=lambda v: True,
     ),
 
+
+    darkcoin_testnet=math.Object(
+        PARENT=networks.nets['darkcoin_testnet'],
+        SHARE_PERIOD=15, # seconds
+        NEW_SHARE_PERIOD=15, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+        SPREAD=30, # blocks
+        NEW_SPREAD=30, # blocks
+        IDENTIFIER='17cf94c1ae12e98f'.decode('hex'),
+        PREFIX='5559f46dfee6881f'.decode('hex'),
+        P2P_PORT=17902,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=13990,
+        BOOTSTRAP_ADDRS=''.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-drk',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
